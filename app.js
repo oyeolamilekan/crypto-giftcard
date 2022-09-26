46b3dc4725ef734d211e73072696587860103af6
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2022-09-27 00:19:16 +0100

    feat: fixing cors error.

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ app.use(cors());
 
 app.use(express.json())
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use("/api", require("./routes/brands.route"))
 
 app.use("/api", require("./routes/crypto.route"))
